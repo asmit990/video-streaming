a8ff0e8d9fc494d2087fa5308ade5aec0ba74fe3
asmit990 <asmitpandeyw4354@gmail.com>
2025-07-07 12:32:04 +0530

    registtion done

--- api/auth/register/route.ts
+++ api/auth/register/route.ts
@@ -5,8 +5,37 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest){
     try{
        const {email, password} =  await request.json()
-    }
-    catch (error){
 
+       if(!email || !password) {
+          return NextResponse.json(
+            {error: "email n password is required"},
+            {status: 400}
+          )
+       }
+       await connectionToDatabase()
+
+       const existingUser = await User.findOne({email})
+       if(existingUser){
+        return NextResponse.json(
+            {error: "user exist"},
+            {status: 400}
+          )
+       }
+      await User.create({
+          email,
+          password
+       })
+
+       return NextResponse.json(
+        {message: "You resgistered"},
+        {status: 201 }
+       );
+    }
+
+    catch (error){
+     return NextResponse.json(
+        {error: "failed to register user"},
+        {status: 400}
+     )
     }
 }
